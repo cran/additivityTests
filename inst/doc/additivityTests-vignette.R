f538f77bcc3a7849366a3c19a0825aeda70cd501
Petr Simecek <simecek@gmail.com>
2022-06-24 11:29:21 +0000

    version 1.1-4.1

--- inst/doc/additivityTests-vignette.R
+++ inst/doc/additivityTests-vignette.R
@@ -1,5 +1,4 @@
-
-## ----'generating data with no interaction'-------------------------------
+## ----'generating data with no interaction'------------------------------------
     require(additivityTests)
     set.seed(123)
     subjects = rnorm(10)
@@ -7,8 +6,7 @@
     noise = rnorm(100)/100
     Y = matrix(rep(subjects,10), 10, 10) + matrix(rep(treatments, each=10), 10, 10) + noise
 
-
-## ----'testing for interaction'-------------------------------------------
+## ----'testing for interaction'------------------------------------------------
     tukey.test(Y)
     mandel.test(Y)
     lbi.test(Y)
@@ -17,8 +15,7 @@
     mandel.test(Y)
     mtukey.test(Y, correction=2, Nboot=1000)
 
-
-## ----'adding interaction and testing again'------------------------------
+## ----'adding interaction and testing again'-----------------------------------
     Y[1:5,] = Y[1:5,] + 10*rep(treatments, each=5)
     
     tukey.test(Y)
@@ -28,5 +25,4 @@
     johnson.graybill.test(Y)
     mandel.test(Y)
     mtukey.test(Y, correction=2, Nboot=1000)
-
 
